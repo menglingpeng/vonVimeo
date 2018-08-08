f398b85f0825ea7067d1fea386fa4b1fcd12b1e9
menglingpeng <menglingpeng@live.com>
2018-08-08 10:04:02 +0800

    feat: init the view of group detail

---
+++ app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/activity/ProjectDetailActivity.java
@@ -0,0 +1,66 @@
+package com.menglingpeng.vonvimeo.mvp.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.menglingpeng.vonvimeo.base.BaseActivity;
+import com.menglingpeng.vonvimeo.mvp.interf.RecyclerView;
+import com.menglingpeng.vonvimeo.mvp.view.RecyclerFragment;
+
+public class ProjectDetailActivity extends BaseActivity implements RecyclerView{
+
+    private Toolbar toolbar;
+    private String type;
+    private String title;
+    private static RecyclerFragment fragment;
+
+    @Override
+    protected void initLayoutId() {
+        layoutId = R.layout.activity_project_detail;
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+        toolbar = (Toolbar) findViewById(R.id.project_detail_tb);
+        toolbar.setTitle(title);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+    
+
+    @Override
+    public void hideProgress() {
+
+    }
+
+    @Override
+    public void loadFailed(String msg) {
+
+    }
+
+    @Override
+    public void loadSuccess(String json, String requestType) {
+
+    }
+}

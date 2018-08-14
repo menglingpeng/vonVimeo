f295b7f3117f55705060a7663334e465e80ccf0e
menglingpeng <menglingpeng@live.com>
2018-08-14 09:27:02 +0800

    feat: add the function of create new album for user collections page

--- app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/activity/UserFollowingActivity.java
+++ app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/activity/UserFollowingActivity.java
@@ -1,25 +1,42 @@
 package com.menglingpeng.vonvimeo.mvp.view.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.TextInputEditText;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import com.menglingpeng.vonvimeo.base.BaseActivity;
 import com.menglingpeng.vonvimeo.mvp.interf.RecyclerView;
+import com.menglingpeng.vonvimeo.mvp.presenter.RecyclerPresenter;
 import com.menglingpeng.vonvimeo.mvp.view.RecyclerFragment;
+import com.menglingpeng.vonvimeo.mvp.view.UserAlbumActivity;
 import com.menglingpeng.vonvimeo.utils.Constants;
+import com.menglingpeng.vonvimeo.utils.SharedPrefUtils;
+import com.menglingpeng.vonvimeo.utils.SnackbarUtils;
+
+import java.util.HashMap;
 
 public class UserFollowingActivity extends BaseActivity implements RecyclerView, View.OnClickListener{
 
+    private CoordinatorLayout coordinatorLayout;
     private Toolbar toolbar;
     private String type;
     private String userName;
     private TextView likesCountTv;
     private TextView collectionCountTv;
     private TextView followingCountTv;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void initLayoutId() {
@@ -33,6 +50,7 @@ public class UserFollowingActivity extends BaseActivity implements RecyclerView,
         userName = getIntent().getStringExtra(Constants.NAME);
         String title = new StringBuilder().append(userName).append(getString(R.string.s)).append(getString(R.string
                 .following)).toString();
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.user_following_cdl);
         toolbar = (Toolbar) findViewById(R.id.user_following_tb);
         likesCountTv = (TextView)findViewById(R.id.likes_count_tv);
         collectionCountTv = (TextView)findViewById(R.id.collection_count_tv);
@@ -55,6 +73,25 @@ public class UserFollowingActivity extends BaseActivity implements RecyclerView,
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.user_likes_toolbar_overflow_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.user_following_sort_date:
+                break;
+            case R.id.user_following_sort_alphabetical:
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onClick(View view) {
         Intent intent;
         switch (view.getId()){
@@ -73,6 +110,48 @@ public class UserFollowingActivity extends BaseActivity implements RecyclerView,
             default:
                 break;
         }
+    }
+
+    private void showCreateAlbumDialog() {
+        final TextInputEditText albumNameEt, albumDescEt;
+        AlertDialog dialog;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View dialogView = getLayoutInflater().inflate(R.layout.create_a_bucket_dialog_message, null);
+        builder.setTitle(R.string.create_a_bucket);
+        builder.setView(dialogView);
+        albumNameEt = (TextInputEditText) dialogView.findViewById(R.id.bucket_name_tiet);
+        albumDescEt = (TextInputEditText) dialogView.findViewById(R.id.bucket_desc_tiet);
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.setPositiveButton(R.string.create, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String name = albumNameEt.getText().toString();
+                if (name.equals("")) {
+                    SnackbarUtils.showSnackShort(getApplicationContext(), coordinatorLayout, getString(R.string
+                            .the_name_of_bucket_is_not_null));
+                } else {
+                    HashMap<String, String> map = new HashMap<>();
+                    map.put(Constants.ACCESS_TOKEN, SharedPrefUtils.getAuthToken());
+                    map.put(Constants.NAME, albumNameEt.getText().toString());
+                    map.put(Constants.DESCRIPTION, albumDescEt.getText().toString());
+                    type = Constants.REQUEST_CREATE_A_ALBUM;
+                    RecyclerPresenter presenter = new RecyclerPresenter(UserAlbumActivity.this, type, Constants
+                            .REQUEST_NORMAL, Constants.REQUEST_POST_MEIHOD, map, getApplicationContext());
+                    presenter.loadJson();
+                    SnackbarUtils.showSnackShort(getApplicationContext(), coordinatorLayout, getString(R.string
+                            .snack_create_a_bucket_text));
+                }
+
+            }
+        });
+        albumNameEt.setFocusable(true);
+        dialog = builder.create();
+        dialog.show();
     }
 
     @Override

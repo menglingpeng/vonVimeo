be7ad10d92e388e5cfaced474b1398f786495c87
menglingpeng <menglingpeng@live.com>
2018-08-16 21:13:01 +0800

    feat: init the view of invite friends to vimeo

---
+++ app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/activity/InviteFriendsActivity.java
@@ -0,0 +1,46 @@
+package com.menglingpeng.vonvimeo.mvp.view.activity;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+
+import com.menglingpeng.vonvimeo.base.BaseActivity;
+import com.menglingpeng.vonvimeo.mvp.view.RecyclerFragment;
+
+public class InviteFriendsActivity extends BaseActivity {
+
+    private Toolbar toolbar;
+    private CoordinatorLayout coordinatorLayout;
+    private String title;
+    private Context context;
+    private EditText editText;
+
+    @Override
+    protected void initLayoutId() {
+        layoutId = R.layout.activity_invite_friends;
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+        context = getApplicationContext();
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.invite_friends_cdl);
+        toolbar = (Toolbar) findViewById(R.id.invite_friends_tb);
+        editText = (EditText)findViewById(R.id.invite_email_et);
+        toolbar.setTitle(title);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+}

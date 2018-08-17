35054dd72fd3b1abdc613df884f0d366a835e1b5
menglingpeng <menglingpeng@live.com>
2018-08-17 13:04:18 +0800

    feat: add the function of send invite for invite friends page

--- app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/activity/InviteFriendsActivity.java
+++ app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/activity/InviteFriendsActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.menglingpeng.vonvimeo.base.BaseActivity;
@@ -19,6 +20,7 @@ public class InviteFriendsActivity extends BaseActivity {
     private String title;
     private Context context;
     private EditText editText;
+    private Button sendInviteBt;
 
     @Override
     protected void initLayoutId() {
@@ -32,6 +34,7 @@ public class InviteFriendsActivity extends BaseActivity {
         coordinatorLayout = (CoordinatorLayout) findViewById(R.id.invite_friends_cdl);
         toolbar = (Toolbar) findViewById(R.id.invite_friends_tb);
         editText = (EditText)findViewById(R.id.invite_email_et);
+        sendInviteBt = (Button)findViewById(R.id.send_invite_bt);
         toolbar.setTitle(title);
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_back);
@@ -40,6 +43,13 @@ public class InviteFriendsActivity extends BaseActivity {
             public void onClick(View v) {
                 finish();
             }
+        });
+        sendInviteBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+            
         });
 
     }

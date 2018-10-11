f89a7db3676678a702131247768f27422fadf033
menglingpeng <menglingpeng@live.com>
2018-10-11 20:56:54 +0800

    feat: add the function of settings for uploading a video

--- app/src/main/java/com/menglingpeng/vonvimeo/mvp/model/LikeVideo.java
+++ app/src/main/java/com/menglingpeng/vonvimeo/mvp/model/LikeVideo.java
@@ -719,6 +719,8 @@ public class LikeVideo implements Serializable {
         public static class InteractionsBean {
 
             private WatchlaterBean watchlater;
+            private LikeBean like;
+            private ReportBean report;
 
             public void setWatchlater(WatchlaterBean watchlater) {
                 this.watchlater = watchlater;
@@ -727,6 +729,23 @@ public class LikeVideo implements Serializable {
             public WatchlaterBean getWatchlater() {
                 return watchlater;
             }
+
+            public void setLike(LikeBean like) {
+                this.like = like;
+            }
+
+            public LikeBean getLike() {
+                return like;
+            }
+
+            public void setReport(ReportBean report) {
+                this.report = report;
+            }
+
+            public ReportBean getReport() {
+                return report;
+            }
+
 
             public static class WatchlaterBean {
 
@@ -765,6 +784,77 @@ public class LikeVideo implements Serializable {
 
                 public List<String> getOptions() {
                     return options;
+                }
+            }
+
+            public static class LikeBean {
+
+                private String uri;
+                private boolean added;
+                private String added_time;
+                private List<String> options;
+
+                public void setUri(String uri) {
+                    this.uri = uri;
+                }
+
+                public void setAdded(boolean added) {
+                    this.added = added;
+                }
+
+                public void setAdded_time(String added_time) {
+                    this.added_time = added_time;
+                }
+
+                public void setOptions(List<String> options) {
+                    this.options = options;
+                }
+
+                public String getUri() {
+                    return uri;
+                }
+
+                public boolean getAdded() {
+                    return added;
+                }
+
+                public String getAdded_time() {
+                    return added_time;
+                }
+
+                public List<String> getOptions() {
+                    return options;
+                }
+            }
+
+            public static class ReportBean {
+
+                private String uri;
+                private List<String> options;
+                private List<String> reason;
+
+                public void setUri(String uri) {
+                    this.uri = uri;
+                }
+
+                public void setOptions(List<String> options) {
+                    this.options = options;
+                }
+
+                public void setReason(List<String> reason) {
+                    this.reason = reason;
+                }
+
+                public String getUri() {
+                    return uri;
+                }
+
+                public List<String> getOptions() {
+                    return options;
+                }
+
+                public List<String> getReason() {
+                    return reason;
                 }
             }
         }

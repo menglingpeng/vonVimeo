a292a40e3d43df8272e09c694439b4ff1e09dda2
menglingpeng <menglingpeng@live.com>
2018-11-10 22:00:05 +0800

    feat: init the view of notifications settings for auth user

---
+++ app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/fragment/NotificationsSettingsFragment.java
@@ -0,0 +1,30 @@
+package com.menglingpeng.vonvimeo.mvp.view.fragment;
+
+import android.content.Context;
+
+import com.menglingpeng.vonvimeo.base.BaseFragment;
+import com.menglingpeng.vonvimeo.mvp.model.User;
+import com.menglingpeng.vonvimeo.utils.Constants;
+
+public class NotificationsSettingsFragment extends BaseFragment {
+
+    private User user;
+    private Context context;
+
+    @Override
+    protected void initLayoutId() {
+        layoutId = R.layout.fragment_notifications_settings;
+    }
+
+    @Override
+    protected void initView() {
+
+        user = (User)getActivity().getIntent().getSerializableExtra(Constants.USER);
+        context = getContext();
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}

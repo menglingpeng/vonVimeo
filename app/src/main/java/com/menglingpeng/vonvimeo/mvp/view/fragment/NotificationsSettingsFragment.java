fc46d3d96b2b9c349d192298dce6b819aaab4caa
menglingpeng <menglingpeng@live.com>
2018-11-11 15:55:07 +0800

    feat: add the settings of On-site activity for NotificationsSettingsFragment

--- app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/fragment/NotificationsSettingsFragment.java
+++ app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/fragment/NotificationsSettingsFragment.java
@@ -1,6 +1,9 @@
 package com.menglingpeng.vonvimeo.mvp.view.fragment;
 
 import android.content.Context;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ListView;
 
 import com.menglingpeng.vonvimeo.base.BaseFragment;
 import com.menglingpeng.vonvimeo.mvp.model.User;
@@ -10,6 +13,8 @@ public class NotificationsSettingsFragment extends BaseFragment {
 
     private User user;
     private Context context;
+    private ListView listView;
+    private CheckBox sendNotificationsCb;
 
     @Override
     protected void initLayoutId() {
@@ -21,10 +26,23 @@ public class NotificationsSettingsFragment extends BaseFragment {
 
         user = (User)getActivity().getIntent().getSerializableExtra(Constants.USER);
         context = getContext();
+        listView= (ListView) rootView.findViewById(R.id.notifications_settings_on_site_activity_lv);
+        sendNotificationsCb = (CheckBox)rootView.findViewById(R.id.
+                notifications_settings_on_site_activity_send_notifications_only_i_follow_cb);
     }
 
     @Override
     protected void initData() {
+        sendNotificationsCb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
 
+                }
+                else {
+                    
+                }
+            }
+        });
     }
 }

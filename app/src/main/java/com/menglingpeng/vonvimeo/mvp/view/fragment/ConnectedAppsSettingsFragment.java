1108696b251b517b420b68d20d9119d926e18374
menglingpeng <menglingpeng@live.com>
2018-11-14 21:30:22 +0800

    init the view of connect vimeo to other accounts

--- app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/fragment/ConnectedAppsSettingsFragment.java
+++ app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/fragment/ConnectedAppsSettingsFragment.java
@@ -1,17 +1,22 @@
 package com.menglingpeng.vonvimeo.mvp.view.fragment;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.menglingpeng.vonvimeo.base.BaseFragment;
 import com.menglingpeng.vonvimeo.mvp.model.User;
 import com.menglingpeng.vonvimeo.utils.Constants;
 
-public class ConnectedAppsSettingsFragment extends BaseFragment {
+public class ConnectedAppsSettingsFragment extends BaseFragment implements View.OnClickListener{
 
     private User user;
     private Context context;
     private ListView listView;
+    private Button connectToFacebookBt;
+    private Button connectToYouTubekBt;
+    private Button connectToTwitterBt;
 
     @Override
     protected void initLayoutId() {
@@ -24,10 +29,27 @@ public class ConnectedAppsSettingsFragment extends BaseFragment {
         user = (User)getActivity().getIntent().getSerializableExtra(Constants.USER);
         context = getContext();
         listView = (ListView)rootView.findViewById(R.id.connected_apps_settings_to_other_accounts_lv);
+        connectToFacebookBt = (Button)rootView.findViewById(R.id.connected_apps_settings_connect_to_facebook_bt);
+        connectToYouTubekBt = (Button)rootView.findViewById(R.id.connected_apps_settings_connect_to_youtube_bt);
+        connectToTwitterBt = (Button)rootView.findViewById(R.id.connected_apps_settings_connect_to_twitter_bt);
     }
 
     @Override
     protected void initData() {
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.connected_apps_settings_connect_to_facebook_bt:
+                break;
+            case R.id.connected_apps_settings_connect_to_youtube_bt:
+                break;
+            case R.id.connected_apps_settings_connect_to_twitter_bt:
+                break;
+            default:
+                break;
+        }
     }
 }

27d45193f76d3e6e5cd2d4aae50e64e840be5a7e
menglingpeng <menglingpeng@live.com>
2018-11-19 14:39:22 +0800

    init the view of email service providers settings for MarketingSettingsFragment

--- app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/fragment/MarketingSettingsFragment.java
+++ app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/fragment/MarketingSettingsFragment.java
@@ -2,6 +2,7 @@ package com.menglingpeng.vonvimeo.mvp.view.fragment;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.Button;
 
 import com.menglingpeng.vonvimeo.base.BaseFragment;
 import com.menglingpeng.vonvimeo.mvp.model.User;
@@ -11,6 +12,10 @@ public class MarketingSettingsFragment extends BaseFragment implements View.OnCl
 
     private User user;
     private Context context;
+    private Button connectToMailChimpBt;
+    private Button connectToConstantContactBt;
+    private Button connectToCampaignMonitorBt;
+    private Button connectToInfusionsoftBt;
 
     @Override
     protected void initLayoutId() {
@@ -21,6 +26,10 @@ public class MarketingSettingsFragment extends BaseFragment implements View.OnCl
     protected void initView() {
         user = (User)getActivity().getIntent().getSerializableExtra(Constants.USER);
         context = getContext();
+        connectToMailChimpBt = (Button)rootView.findViewById(R.id.connected_apps_settings_connect_to_mailchimp_bt);
+        connectToCampaignMonitorBt = (Button)rootView.findViewById(R.id.connected_apps_settings_connect_to_campaign_monitor_bt);
+        connectToConstantContactBt = (Button)rootView.findViewById(R.id.connected_apps_settings_connect_to_constant_contact_bt);
+        connectToInfusionsoftBt = (Button)rootView.findViewById(R.id.connected_apps_settings_connect_to_infusionsoft_bt);
     }
 
     @Override
@@ -30,6 +39,8 @@ public class MarketingSettingsFragment extends BaseFragment implements View.OnCl
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()){
 
+        }
     }
 }

809c06254d7af1c03cff5fb82169d4894f4c19ad
menglingpeng <menglingpeng@live.com>
2018-11-21 23:02:56 +0800

    feat: add the settings function of upload default for VideosSettingsFragment

--- app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/fragment/VideosSettingsFragment.java
+++ app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/fragment/VideosSettingsFragment.java
@@ -1,6 +1,12 @@
 package com.menglingpeng.vonvimeo.mvp.view.fragment;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.menglingpeng.vonvimeo.base.BaseFragment;
 import com.menglingpeng.vonvimeo.mvp.model.User;
@@ -10,6 +16,12 @@ public class VideosSettingsFragment extends BaseFragment {
 
     private User user;
     private Context context;
+    private RadioGroup contentRatingRg;
+    private RadioButton noRatingRb;
+    private RadioButton allAudiencesRb;
+    private RadioButton matureRb;
+    private Button contentRatingSaveBt;
+    private CheckBox videoPreferencesCb;
 
     @Override
     protected void initLayoutId() {
@@ -20,10 +32,43 @@ public class VideosSettingsFragment extends BaseFragment {
     protected void initView() {
         user = (User)getActivity().getIntent().getSerializableExtra(Constants.USER);
         context = getContext();
+        contentRatingRg = (RadioGroup)rootView.findViewById(R.id.videos_settings_content_rating_default_rg);
+        noRatingRb = (RadioButton)rootView.findViewById(R.id.videos_settings_content_rating_default_no_rating_rb);
+        allAudiencesRb = (RadioButton)rootView.findViewById(R.id.videos_settings_content_rating_default_all_audiences_rb);
+        matureRb = (RadioButton)rootView.findViewById(R.id.videos_settings_content_rating_default_mature_rb);
+        contentRatingSaveBt = (Button) rootView.findViewById(R.id.videos_settings_content_rating_default_save_bt);
+        videoPreferencesCb = (CheckBox)rootView.findViewById(R.id.videos_settings_video_preferences_cb);
     }
 
     @Override
     protected void initData() {
+        contentRatingRg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i){
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        break;
 
+                }
+            }
+        });
+        contentRatingSaveBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        videoPreferencesCb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+            }
+        });
     }
 }

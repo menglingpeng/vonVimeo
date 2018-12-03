570a9798a064cb6d58ed90cd765e3f01b779c482
menglingpeng <menglingpeng@live.com>
2018-12-03 22:12:00 +0800

    feat: add the helper utils of Alipay for UpgradeSettingsFragment

---
+++ app/src/main/java/com/menglingpeng/vonvimeo/utils/AliayOrderInfoUtil.java
@@ -0,0 +1,42 @@
+package com.menglingpeng.vonvimeo.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Random;
+
+public class AliayOrderInfoUtil {
+
+
+    /**
+     * 构造支付订单参数列表
+     */
+    public static Map<String, String> buildOrderParamMap(String app_id, boolean rsa2, String price) {
+        Map<String, String> keyValues = new HashMap<String, String>();
+        keyValues.put("app_id", app_id);
+        keyValues.put("biz_content", bizCotent(price));
+        keyValues.put("charset", "utf-8");
+        keyValues.put("method", "alipay.trade.app.pay");
+        keyValues.put("sign_type", rsa2 ? "RSA2" : "RSA");
+        keyValues.put("timestamp", "2016-07-29 16:55:53");
+        keyValues.put("version", "1.0");
+        return keyValues;
+    }
+
+    /**
+     * 要求外部订单号必须唯一。
+     */
+    public static String getOutTradeNo() {
+        SimpleDateFormat format = new SimpleDateFormat("MMddHHmmss", Locale.getDefault());
+        Date date = new Date();
+        String key = format.format(date);
+
+        Random r = new Random();
+        key = key + r.nextInt();
+        key = key.substring(0, 15);
+        return key;
+    }
+
+}
---
+++ app/src/main/java/com/menglingpeng/vonvimeo/utils/AlipayConfig.java
@@ -0,0 +1,35 @@
+package com.menglingpeng.vonvimeo.utils;
+
+public class AlipayConfig {
+
+    /**
+     * 支付宝支付业务：入参app_id，上文创建应用时候，已经得到
+     * 由于App支付功能需要签约，因此需要上传公司信息和证件等资料进行签约
+     * 以下参数，由上传完整公司信息后即可得到
+     */
+    public static final String APPID = "XXXXXXXXXXXXX";
+    // 商户PID
+    public static final String PARTNER = "XXXXXXXXXXXXX";
+
+    // 商户收款账号
+
+    public static final String SELLER = "XXXXXXXXXX";
+    /**
+     * 支付宝账户登录授权业务：入参target_id值
+     * 可以用时间戳
+     */
+    public static final String TARGET_ID = AliayOrderInfoUtil.getOutTradeNo();
+
+    /** 商户私钥，pkcs8格式 */
+    /** 如下私钥，RSA2_PRIVATE 或者 RSA_PRIVATE 只需要填入一个 */
+    /** 如果商户两个都设置了，优先使用 RSA2_PRIVATE */
+    /** RSA2_PRIVATE 可以保证商户交易在更加安全的环境下进行，建议使用 RSA2_PRIVATE */
+    /** 获取 RSA2_PRIVATE，建议使用支付宝提供的公私钥生成工具生成， */
+    /**
+     *  使用支付宝提供的工具生成RSA公钥和私钥
+     *  工具地址：https://doc.open.alipay.com/docs/doc.htmtreeId=291&articleId=106097&docType=1
+     */
+    public static final String RSA2_PRIVATE = "";
+    public static final String RSA_PRIVATE = "";
+
+}

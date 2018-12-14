934eb5230855d05aee88e8cba81c04d04386adfe
menglingpeng <menglingpeng@live.com>
2018-12-14 22:01:43 +0800

    feat: add the function of decimal point key

--- app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/custom/ReflectionUtils.java
+++ app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/custom/ReflectionUtils.java
@@ -3,8 +3,63 @@ package com.menglingpeng.vonvimeo.mvp.view.custom;
 import android.text.TextUtils;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class ReflectionUtils {
+
+
+    /**
+     * 循环向上转型, 获取对象的 DeclaredMethod
+     *
+     * @param object         : 子类对象
+     * @param methodName     : 父类中的方法名
+     * @param parameterTypes : 父类中的方法参数类型
+     * @return 父类中的方法对象
+     */
+
+    public static Method getDeclaredMethod(Object object, String methodName, Class<?>... parameterTypes) {
+        Method method = null;
+
+        for (Class<?> clazz = object.getClass(); clazz != Object.class; clazz = clazz.getSuperclass()) {
+            try {
+                method = clazz.getDeclaredMethod(methodName, parameterTypes);
+                return method;
+            } catch (Exception e) {
+                //这里甚么都不要做！并且这里的异常必须这样写，不能抛出去。
+                //如果这里的异常打印或者往外抛，则就不会执行clazz = clazz.getSuperclass(),最后就不会进入到父类中了
+
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * 循环向上转型, 获取对象的 DeclaredField
+     *
+     * @param object    : 子类对象
+     * @param fieldName : 父类中的属性名
+     * @return 父类中的属性对象
+     */
+
+    public static Field getDeclaredField(Object object, String fieldName) {
+        Field field = null;
+
+        Class<?> clazz = object.getClass();
+
+        for (; clazz != Object.class; clazz = clazz.getSuperclass()) {
+            try {
+                field = clazz.getDeclaredField(fieldName);
+                return field;
+            } catch (Exception e) {
+                //这里甚么都不要做！并且这里的异常必须这样写，不能抛出去。
+                //如果这里的异常打印或者往外抛，则就不会执行clazz = clazz.getSuperclass(),最后就不会进入到父类中了
+
+            }
+        }
+
+        return null;
+    }
 
     public static Object getFieldValue(Object obj, String fieldName) {
         if (obj == null || TextUtils.isEmpty(fieldName)) {

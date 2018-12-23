f40fed0fd1dceb2ff6567e0937839320d877d32e
menglingpeng <menglingpeng@live.com>
2018-12-23 22:12:03 +0800

    feat: add the function of share to wechat for SupportedBankCardActivity

---
+++ app/src/main/java/com/menglingpeng/vonvimeo/utils/wxshare/WechatShareManager.java
@@ -0,0 +1,153 @@
+package com.menglingpeng.vonvimeo.utils.wxshare;
+
+import android.content.Context;
+
+import com.tencent.mm.opensdk.modelmsg.SendMessageToWX;
+import com.tencent.mm.opensdk.modelmsg.WXMediaMessage;
+import com.tencent.mm.opensdk.modelmsg.WXTextObject;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
+public class WechatShareManager {
+
+    private static WechatShareManager mInstance;
+    private ShareContent mShareContentText, mShareContentPicture, mShareContentWebpag, mShareContentVideo;
+    private IWXAPI mWXApi;
+    private Context mContext;
+
+    public static final int WECHAT_SHARE_WAY_TEXT = 1;   //文字
+    public static final int WECHAT_SHARE_WAY_PICTURE = 2; //图片
+    public static final int WECHAT_SHARE_WAY_WEBPAGE = 3;  //链接
+    public static final int WECHAT_SHARE_WAY_VIDEO = 4; //视频
+    public static final int WECHAT_SHARE_TYPE_TALK = SendMessageToWX.Req.WXSceneSession;  //会话
+    public static final int WECHAT_SHARE_TYPE_FRENDS = SendMessageToWX.Req.WXSceneTimeline; //朋友圈
+
+    private WechatShareManager(Context context){
+        this.mContext = context;
+        //初始化数据
+        //初始化微信分享代码
+        initWechatShare(context);
+    }
+
+    /**
+     * 获取WeixinShareManager实例
+     * 非线程安全，请在UI线程中操作
+     * @return
+     */
+
+
+    public static WechatShareManager getInstance(Context context){
+        if(mInstance == null){
+            mInstance = new WechatShareManager(context);
+        }
+        return mInstance;
+    }
+
+    private void initWechatShare(Context context){
+        if (mWXApi == null) {
+            mWXApi = WXAPIFactory.createWXAPI(context, WechatShareUtil.WECHAT_APP_ID, true);
+        }
+        mWXApi.registerApp(WechatShareUtil.WECHAT_APP_ID);
+    }
+    /**
+     * 通过微信分享
+     * @param shareContent 分享的方式（文本、图片、链接）
+     * @param shareType 分享的类型（朋友圈，会话）
+     */
+    public void shareByWebchat(ShareContent shareContent, int shareType){
+        switch (shareContent.getShareWay()) {
+            case WECHAT_SHARE_WAY_TEXT:
+                shareText(shareContent, shareType);
+                break;
+            case WECHAT_SHARE_WAY_PICTURE:
+                break;
+            case WECHAT_SHARE_WAY_WEBPAGE:
+                break;
+            case WECHAT_SHARE_WAY_VIDEO:
+                break;
+        }
+    }
+
+    private abstract class ShareContent {
+        protected abstract int getShareWay();
+        protected abstract String getContent();
+        protected abstract String getTitle();
+        protected abstract String getURL();
+        protected abstract int getPictureResource();
+    }
+
+    /**
+     * 设置分享文字的内容
+     * @author chengcj1
+     *
+     */
+    public class ShareContentText extends ShareContent {
+        private String content;
+
+        /**
+         * 构造分享文字类
+         * @param text 分享的文字内容
+         */
+        public ShareContentText(String content){
+            this.content = content;
+        }
+
+        @Override
+        protected int getShareWay() {
+            return WECHAT_SHARE_WAY_TEXT;
+        }
+
+        @Override
+        protected String getContent() {
+            return content;
+        }
+
+        @Override
+        protected String getTitle() {
+            return null;
+        }
+
+        @Override
+        protected String getURL() {
+            return null;
+        }
+
+        @Override
+        protected int getPictureResource() {
+            return -1;
+        }
+    }
+
+    /*
+     * 获取文本分享对象
+     */
+    public ShareContent getShareContentText(String content) {
+        if (mShareContentText == null) {
+            mShareContentText = new ShareContentText(content);
+        }
+        return (ShareContentText) mShareContentText;
+    }
+
+    /*
+     * 分享文字
+     */
+    private void shareText(ShareContent shareContent, int shareType) {
+        String text = shareContent.getContent();
+        //初始化一个WXTextObject对象
+        WXTextObject textObj = new WXTextObject();
+        textObj.text = text;
+        //用WXTextObject对象初始化一个WXMediaMessage对象
+        WXMediaMessage msg = new WXMediaMessage();
+        msg.mediaObject = textObj;
+        msg.description = text;
+        //构造一个Req
+        SendMessageToWX.Req req = new SendMessageToWX.Req();
+        //transaction字段用于唯一标识一个请求
+        req.transaction = buildTransaction("textshare");
+        req.message = msg;
+        //发送的目标场景， 可以选择发送到会话 WXSceneSession 或者朋友圈 WXSceneTimeline。 默认发送到会话。
+        req.scene = shareType;
+        mWXApi.sendReq(req);
+    }
+
+}

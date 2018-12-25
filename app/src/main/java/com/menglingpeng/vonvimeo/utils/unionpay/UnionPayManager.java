d16830ce633a67b0891fd683e91a7fe62ea13f04
menglingpeng <menglingpeng@live.com>
2018-12-25 21:59:35 +0800

    add the base config of unionpay

---
+++ app/src/main/java/com/menglingpeng/vonvimeo/utils/unionpay/UnionPayManager.java
@@ -0,0 +1,77 @@
+package com.menglingpeng.vonvimeo.utils.unionpay;
+
+import android.content.DialogInterface;
+import android.os.Message;
+import android.support.v7.app.AlertDialog;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class UnionPayManager {
+
+
+    private static final String TN_URL_01 = "http://101.231.204.84:8091/sim/getacptn";
+    //访问url  获取tn
+    @Override
+    public void run() {
+        String tn = null;
+        InputStream is;
+        try {
+
+            String url = TN_URL_01;
+
+            URL myURL = new URL(url);
+            URLConnection ucon = myURL.openConnection();
+            ucon.setConnectTimeout(120000);
+            is = ucon.getInputStream();
+            int i = -1;
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            while ((i = is.read()) != -1) {
+                baos.write(i);
+            }
+
+            tn = baos.toString();
+            is.close();
+            baos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Message msg = mHandler.obtainMessage();
+        msg.obj = tn;
+        mHandler.sendMessage(msg);
+    }
+
+    //获取tn
+    @Override
+    public boolean handleMessage(Message msg) {
+        if (mLoadingDialog.isShowing()) {
+            mLoadingDialog.dismiss();
+        }
+
+        String tn = "";
+        if (msg.obj == null || ((String) msg.obj).length() == 0) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("错误提示");
+            builder.setMessage("网络连接失败,请重试!");
+            builder.setNegativeButton("确定",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            builder.create().show();
+        } else {
+            tn = (String) msg.obj;
+            /*************************************************
+             * 步骤2：通过银联工具类启动支付插件
+             ************************************************/
+            doStartUnionPayPlugin(this, tn, mMode);
+        }
+
+        return false;
+    }
+}

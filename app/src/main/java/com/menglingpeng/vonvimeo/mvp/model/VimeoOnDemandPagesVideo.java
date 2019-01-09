69bbaaa9d8ba27612c2f4bf3b1b431030c30b9e8
menglingpeng <menglingpeng@live.com>
2019-01-09 21:42:52 +0800

    feat: add the sort function for RegionDetailActivity

--- app/src/main/java/com/menglingpeng/vonvimeo/mvp/model/VimeoOnDemandPagesVideo.java
+++ app/src/main/java/com/menglingpeng/vonvimeo/mvp/model/VimeoOnDemandPagesVideo.java
@@ -2,6 +2,7 @@ package com.menglingpeng.vonvimeo.mvp.model;
 
 import java.io.Serializable;
 import java.security.SecureRandom;
+import java.util.List;
 
 public class VimeoOnDemandPagesVideo implements Serializable{
 
@@ -19,6 +20,7 @@ public class VimeoOnDemandPagesVideo implements Serializable{
     private String release_time;
     private Object license;
     private PrivacyBean privacy;
+    private PicturesBean pictures;
 
     public String getUri() {
         return uri;
@@ -130,6 +132,14 @@ public class VimeoOnDemandPagesVideo implements Serializable{
 
     public void setPrivacy(PrivacyBean privacy) {
         this.privacy = privacy;
+    }
+
+    public PicturesBean getPictures() {
+        return pictures;
+    }
+
+    public void setPictures(PicturesBean pictures) {
+        this.pictures = pictures;
     }
 
     public static class EmbedBean {
@@ -311,6 +321,96 @@ public class VimeoOnDemandPagesVideo implements Serializable{
 
         public String getComments() {
             return comments;
+        }
+    }
+
+    public static class PicturesBean {
+
+        private String uri;
+        private boolean active;
+        private String type;
+        private List<SizesBean> sizes;
+        private String resource_key;
+
+        public void setUri(String uri) {
+            this.uri = uri;
+        }
+
+        public void setActive(boolean active) {
+            this.active = active;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public void setSizes(List<SizesBean> sizes) {
+            this.sizes = sizes;
+        }
+
+        public String getUri() {
+            return uri;
+        }
+
+        public boolean getActive() {
+            return active;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public List<SizesBean> getSizes() {
+            return sizes;
+        }
+
+        public void setResource_key(String resource_key) {
+            this.resource_key = resource_key;
+        }
+
+        public String getResource_key() {
+            return resource_key;
+        }
+
+
+        public static class SizesBean {
+
+            private int width;
+            private int height;
+            private String link;
+            private String link_with_play_button;
+
+            public void setWidth(int width) {
+                this.width = width;
+            }
+
+            public void setHeight(int height) {
+                this.height = height;
+            }
+
+            public void setLink(String link) {
+                this.link = link;
+            }
+
+            public void setLink_with_play_button(String link_with_play_button) {
+                this.link_with_play_button = link_with_play_button;
+            }
+
+            public int getWidth() {
+                return width;
+            }
+
+            public int getHeight() {
+                return height;
+            }
+
+            public String getLink() {
+                return link;
+            }
+
+            public String getLink_with_play_button() {
+                return link_with_play_button;
+            }
         }
     }
 }
--- app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/activity/RegionDetailActivity.java
+++ app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/activity/RegionDetailActivity.java
@@ -1,10 +1,13 @@
 package com.menglingpeng.vonvimeo.mvp.view.activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,6 +17,7 @@ import android.widget.TextView;
 import com.menglingpeng.vonvimeo.base.BaseActivity;
 import com.menglingpeng.vonvimeo.mvp.model.User;
 import com.menglingpeng.vonvimeo.mvp.view.RecyclerFragment;
+import com.menglingpeng.vonvimeo.mvp.view.SearchActivity;
 import com.menglingpeng.vonvimeo.utils.Constants;
 import com.menglingpeng.vonvimeo.utils.IdStringUtil;
 import com.menglingpeng.vonvimeo.utils.ImageLoader;
@@ -25,6 +29,7 @@ public class RegionDetailActivity extends BaseActivity {
     private String title;
     private Context context;
     private ProgressBar progressBar;
+    private String sortType;
 
     @Override
     protected void initLayoutId() {
@@ -48,5 +53,41 @@ public class RegionDetailActivity extends BaseActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.region_detail_toolbar_overflow_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.region_detail_search:
+                Intent intent = new Intent(this, SearchActivity.class);
+                intent.putExtra(Constants.ACTIVITY, Constants.ACTIVITY_VIMEO_ONDEMAND_PAGES);
+                startActivity(intent);
+                break;
+            case R.id.region_detail_sort_date:
+                sortType = Constants.TYPE_DATE;
+                break;
+            case R.id.region_detail_sort_alphabetical:
+                sortType = Constants.TYPE_ALPHABETICAL;
+                break;
+            case R.id.region_detail_sort_videos:
+                sortType = Constants.TYPE_VIDEOS;
+                break;
+            case R.id.region_detail_sort_comments:
+                sortType = Constants.TYPE_COMMENTS;
+                break;
+            case R.id.region_detail_thumb_view:
+                break;
+            case R.id.region_detail_detail_view:
+                break;
+            default:
+                break;
+        }
     }
 }

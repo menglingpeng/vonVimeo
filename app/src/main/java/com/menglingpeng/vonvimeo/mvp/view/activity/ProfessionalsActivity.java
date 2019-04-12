fadc9310aa38fe9202ac298d8e9fc9bb9f35c4de
menglingpeng <menglingpeng@live.com>
2019-04-12 18:37:50 +0800

    init the tablayout and viewpager of ProfessionalsActivity

--- app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/activity/ProfessionalsActivity.java
+++ app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/activity/ProfessionalsActivity.java
@@ -2,16 +2,25 @@ package com.menglingpeng.vonvimeo.mvp.view.activity;
 
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.menglingpeng.vonvimeo.base.BaseActivity;
+import com.menglingpeng.vonvimeo.mvp.adapter.TabPagerFragmentAdapter;
 import com.menglingpeng.vonvimeo.mvp.model.User;
+import com.menglingpeng.vonvimeo.mvp.view.RecyclerFragment;
 import com.menglingpeng.vonvimeo.utils.Constants;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ProfessionalsActivity extends BaseActivity {
 
@@ -23,6 +32,13 @@ public class ProfessionalsActivity extends BaseActivity {
     private Button getPROBt;
     private Context context;
     private User user;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ProgressBar progressBar;
+    private TabPagerFragmentAdapter adapter;
+    private HashMap<String, String> map;
+    private static final int SMOOTHSCROLL_TOP_POSITION = 50;
+
 
 
     @Override
@@ -40,5 +56,48 @@ public class ProfessionalsActivity extends BaseActivity {
         descTv = (TextView)findViewById(R.id.professionals_desc_tv);
         backgroundIv = (ImageView) findViewById(R.id.professionals_backgroud_iv);
         getPROBt = (Button) findViewById(R.id.professionals_get_pro_bt);
+        progressBar = (ProgressBar)findViewById(R.id.professionals_pb);
+        initTabPager();
+    }
+
+
+    private void initTabPager() {
+        tabLayout = (TabLayout)findViewById(R.id.channel_detail_tl);
+        viewPager = (ViewPager)findViewById(R.id.channel_detail_vp);
+        adapter = new TabPagerFragmentAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                scrollToTop(fragmentsList.get(tab.getPosition()).getRecyclerView());
+            }
+        });
+
+    }
+
+    private void scrollToTop(android.support.v7.widget.RecyclerView list) {
+        int lastPosition;
+        if (null != list) {
+            LinearLayoutManager manager = (LinearLayoutManager) list.getLayoutManager();
+            lastPosition = manager.findLastVisibleItemPosition();
+            if (lastPosition < SMOOTHSCROLL_TOP_POSITION) {
+                list.smoothScrollToPosition(0);
+            } else {
+                list.scrollToPosition(0);
+            }
+        }
     }
 }

118b3143fd94d1a1aabdbd3e49f5b36a4714e50a
menglingpeng <menglingpeng@live.com>
2019-04-23 11:04:45 +0800

    feat: add the description of become a partner for VimeoPartnersActivity

--- app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/activity/VimeoPartnersActivity.java
+++ app/src/main/java/com/menglingpeng/vonvimeo/mvp/view/activity/VimeoPartnersActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
@@ -17,7 +19,9 @@ public class VimeoPartnersActivity extends BaseActivity {
     private CoordinatorLayout coordinatorLayout;
     private String title;
     private Context context;
+    private Button becomePartnerBt;
     private ListView listView;
+    private ListAdapter adapter;
 
     @Override
     protected void initLayoutId() {
@@ -30,6 +34,7 @@ public class VimeoPartnersActivity extends BaseActivity {
         context = getApplicationContext();
         coordinatorLayout = (CoordinatorLayout) findViewById(R.id.vimeo_partners_cdl);
         toolbar = (Toolbar) findViewById(R.id.vimeo_partners_tb);
+        becomePartnerBt = (Button) findViewById(R.id.vimeo_partners_become_a_partner_bt);
         listView = (ListView)findViewById(R.id.vimeo_partners_lv);
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_back);
@@ -37,6 +42,13 @@ public class VimeoPartnersActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        becomePartnerBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
     }
